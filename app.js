a10ec629e7308f1f5de02c7d48ad07c34f8ad4ac
unknown <tayloreldonjohnson@gmail.com>
2017-07-18 13:09:55 -0700

    updated html

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ var compPick = ["rock", "paper", "scissors"];
 var compChoice = function () {
     var finalPick = compPick[Math.floor(Math.random() * 3)];
     console.log(finalPick);  
-
 }
 
 compChoice();
 
+// User clicks buttons to select rock, paper, or scissors
+
+var x = getElementById("Rock");
+var y = getElementById("Paper");
+var z = getElementById("Scissors");
+
+function userPick(x,y,z){
+
+}
